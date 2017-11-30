64bbb1e972404d36b5f894472707248b9ae8891a
Piotr Wojtczak <piotr.m.wojtczak@gmail.com>
2017-11-30 02:09:28 +0100

    Small cleanup in main.py
    Got rid of the unnecessary comments and some unused commented code

--- main.py
+++ main.py
@@ -11,14 +11,10 @@ import messages
 def main():
     """This is the main function"""
 
+    # Getting the settings fot the current gameplay from the setup window
     playerOneStrategy, playerOneColor, playerTwoStrategy, playerTwoColor, rows, cols = settingsDraw()
 
-    """ Temporary input
-    rows = int(input("rows: "))
-    cols = int(input("columns: "))
-    """
-    # Testing the gameplay
-
+    # Setting up the game
     width = min(cols*100, MAX_WIDTH)
     height = (width / cols) * rows
     gameWindow = GraphWin("Connect Four", width, height)
@@ -29,9 +25,9 @@ def main():
     player1 = Player(1, playerOneColor, playerOneStrategy, board)
     player2 = Player(2, playerTwoColor, playerTwoStrategy, board)
 
-    # Actual game
+    # Main game loop
     while not gameWindow.isClosed():
-        # TODO: Maybe move the isOver check to the move() function
+
         player1.move()
         if board.isOver():
             break
@@ -43,5 +39,6 @@ def main():
     gameWindow.close()
 
 
+# Running the main function
 if __name__ == "__main__":
     main()
